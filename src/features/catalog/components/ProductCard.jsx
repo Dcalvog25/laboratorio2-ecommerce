765e212cfa9c103fbff7bb44c8747d02907b038d
johnsydev <johnsylopez02@gmail.com>
2026-08-31 00:32:14 -0600

    Mejora en grid y en la barra de búsqueda
    Co-authored-by: David Calvo Garcia <davidcg2508@gmail.com>

--- src/features/catalog/components/ProductCard.jsx
+++ src/features/catalog/components/ProductCard.jsx
@@ -1,29 +1,53 @@
 import "../styles/ProductCard.css";
 import { ShoppingCart } from "lucide-react";
+import { useState, useEffect } from "react";
 
 export default function ProductCard({ hit }) {
-  return (
-    <div className="product-card">
-      <div className="product-img-container">
-        <img
-          src={hit.image_url}
-          alt={hit.title}
-          className="product-img"
-        />
-      </div>
+  
+    const [tieneDescuento, setTieneDescuento] = useState(false);
 
-      <div className="product-info">
+    useEffect(() => {
+        if (hit.b2c.discount_percentage > 0) {
+            setTieneDescuento(true);
+        }
+    }, []);
+
+    return (
+    <div className="product-card">
+        <div className="product-img-container">
+        <img
+            src={hit.image_url}
+            alt={hit.title}
+            className="product-img"
+        />
+        </div>
+
+        <div className="product-info">
         <h3 className="product-title">{hit.title}</h3>
 
         <p className="product-price">
-          ₡{Number(hit.price).toLocaleString("es-CR")}
+            {tieneDescuento && (
+                <>
+                    <span className="product-discount">
+                        ₡{Number(hit.b2c.sale_price).toLocaleString("en-US")}
+                    </span>
+
+                    <span className="product-regular-price-discount">
+                        ₡{Number(hit.b2c.regular_price).toLocaleString("en-US")}
+                    </span>
+                </>
+            )}
+            {!tieneDescuento && (
+                <span className="product-regular-price">
+                    ₡{Number(hit.b2c.regular_price).toLocaleString("en-US")}
+                </span>
+            )}
         </p>
 
         <button className="product-button">
-          <ShoppingCart size={22} strokeWidth={2} /> Agregar al carrito
+            <ShoppingCart size={22} strokeWidth={2} /> Agregar al carrito
         </button>
-        
-      </div>
+        </div>
     </div>
-  );
+    );
 }
--- src/features/catalog/components/SearchBar.jsx
+++ src/features/catalog/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import { SearchBox } from "react-instantsearch";
+import "../styles/SearchBar.css";
 
 export default function SearchBar() {
   return (
     <div className="search-bar">
-      <SearchBox placeholder="Buscar productos..." />
+      <SearchBox className="search-input" placeholder="Buscar productos..." />
     </div>
   );
 }
