964c879f2b8307c301daa9ec3004f75f17f8b162
johnsydev <johnsylopez02@gmail.com>
2026-08-31 00:59:57 -0600

    Se agrega mensaje e imagen al no obtener resultados de búsqueda
    Co-authored-by: Erincam09 <Erincam09@users.noreply.github.com>

--- src/features/catalog/components/ProductGrid.jsx
+++ src/features/catalog/components/ProductGrid.jsx
@@ -1,15 +1,25 @@
-import { Hits } from "react-instantsearch";
+import { Hits, useInstantSearch } from "react-instantsearch";
 import ProductCard from "./ProductCard";
 import "../styles/ProductGrid.css";
+import noResultsImage from "../../../assets/electroTriste.png";
 
 export default function ProductGrid() {
+    const { results } = useInstantSearch();
+
     return (
         <div className="product-grid">
-            <Hits
-                hitComponent={(props) => {
-                    return <ProductCard {...props} />;
-                }}
-            />
+            {results?.nbHits === 0 ? (
+                <>
+                    <p className="no-results-found">No se encontraron productos.</p>
+                    <img className="no-results-image" src={noResultsImage} alt="No results" />
+                </>
+            ) : (
+                <Hits
+                    hitComponent={(props) => (
+                        <ProductCard {...props} />
+                    )}
+                />
+            )}
         </div>
     );
 }
