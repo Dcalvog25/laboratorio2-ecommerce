99b1ee0a21090d99051508e9da56321e2890860d
johnsydev <johnsylopez02@gmail.com>
2026-09-05 17:53:34 -0600

    Inicio de búsqueda avanzada con autocompletado básico

--- src/features/catalog/components/SearchBar.jsx
+++ src/features/catalog/components/SearchBar.jsx
@@ -1,10 +1,23 @@
-import { SearchBox } from "react-instantsearch";
+import { Autocomplete } from "react-instantsearch";
 import "../styles/SearchBar.css";
 
 export default function SearchBar() {
   return (
     <div className="search-bar">
-      <SearchBox className="search-input" placeholder="Buscar productos..." />
+      <Autocomplete
+        placeholder="Buscar productos..."
+        indices={[
+          {
+            indexName: import.meta.env.VITE_ALGOLIA_INDEX_NAME,
+            itemComponent: ({ item }) => (
+              <div>
+                <h3>{item?.title}</h3>
+                <p>{item?.model}</p>
+              </div>
+            ),
+          },
+        ]}
+      />
     </div>
   );
 }
