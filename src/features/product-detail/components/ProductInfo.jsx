42ce7d7b85eb703c400db4b470643c4d2e2805de
johnsydev <johnsylopez02@gmail.com>
2026-09-05 14:19:33 -0600

    Badge de envío gratis, validación en contador de carrito, mejoras visuales con íconos

--- src/features/product-detail/components/ProductInfo.jsx
+++ src/features/product-detail/components/ProductInfo.jsx
@@ -1,17 +1,19 @@
 import "../styles/ProductInfo.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { ShoppingCart, ShieldCheck, Truck } from "lucide-react";
+import { ShoppingCart, ShieldCheck, Truck, BadgePercent, Clock8 } from "lucide-react";
 import translations from "../../../locales/es.json";
 import Rating from "./Rating";
 import StoresStock from "./StoresStock";
 
 export default function ProductInfo({ product }) {
-    const [cantidadCarrito, setCantidadCarrito] = useState(98);
+    const [cantidadCarrito, setCantidadCarrito] = useState(1);
 
     const navigate = useNavigate();
 
     const tieneDescuento = product?.b2c?.discount_percentage > 0;
+
+    const currency = product?.currency != "CRC" ? "$" : "₡";
 
     return (
     <div className="pi-page">
@@ -53,7 +55,7 @@ export default function ProductInfo({ product }) {
 
                         <div className="pi-product-badges">
                             <span className="pi-product-warranty pi-product-badge"><ShieldCheck /> Garantía de {product.b2c.warranty_months} meses</span>
-                            <span className="pi-product-estimated-delivery pi-product-badge"><Truck /> Tiempo de entrega estimado de {product.b2c.estimated_delivery_days} días</span>
+                            <span className="pi-product-estimated-delivery pi-product-badge"><Clock8 /> Tiempo de entrega estimado de {product.b2c.estimated_delivery_days} días</span>
                         </div>
 
                         <div className="pi-product-info-container-details">
@@ -73,29 +75,34 @@ export default function ProductInfo({ product }) {
                         <div className="pi-container-prices">
                             {tieneDescuento ? (
                                 <>
-                                    <span className="pi-product-price">₡{Number(product.b2c.sale_price).toLocaleString("en-US")}</span>
-                                    <span className="pi-product-regular-price-discount">₡{Number(product.b2c.regular_price).toLocaleString("en-US")}</span>
+                                    <span className="pi-product-price">{currency}{Number(product.b2c.sale_price).toLocaleString("en-US")}</span>
+                                    <span className="pi-product-regular-price-discount">{currency}{Number(product.b2c.regular_price).toLocaleString("en-US")}</span>
 
-                                    <span className="pi-product-discount-badge">¡{product.b2c.discount_percentage}% de descuento!</span>
+
+                                    <span className="pi-product-discount-badge"><BadgePercent /> ¡{product.b2c.discount_percentage}% de descuento!</span>
+                                    {product.b2c.free_shipping && <span className="pi-product-discount-badge"><Truck />¡Envío gratis!</span>}
                                 </>
                             ) : (
-                                <span className="pi-product-price">₡{Number(product.b2c.sale_price).toLocaleString("en-US")}</span>
+                                <>
+                                    <span className="pi-product-price">{currency}{Number(product.b2c.sale_price).toLocaleString("en-US")}</span>
+                                    {product.b2c.free_shipping && <span className="pi-product-discount-badge"><Truck />¡Envío gratis!</span>}
+                                </>
                             )}
                         </div>
                         
-                        {product.in_stock ? (
+                        {(product.in_stock && product.b2c.enabled) ? (
                             <div className="pi-shipping-container">
-                            <div className="pi-shipping-quantity-container">
-                                <button className="pi-shipping-button-del" onClick={() => setCantidadCarrito(cantidadCarrito - 1)}>-</button>
-                                <span className="pi-shipping-quantity">{cantidadCarrito}</span>
-                                <button className="pi-shipping-button-add" onClick={() => setCantidadCarrito(cantidadCarrito + 1)}>+</button>
-                            </div>
+                                <div className="pi-shipping-quantity-container">
+                                    <button className="pi-shipping-button-del" onClick={() => setCantidadCarrito(cantidadCarrito - 1)} disabled={cantidadCarrito === 1}>-</button>
+                                    <span className="pi-shipping-quantity">{cantidadCarrito}</span>
+                                    <button className="pi-shipping-button-add" onClick={() => setCantidadCarrito(cantidadCarrito + 1)} disabled={cantidadCarrito >= product.stock_quantity}>+</button>
+                                </div>
 
-                            <button className="pi-add-to-cart"><ShoppingCart /> Agregar al carrito</button>
-                        </div>
+                                <button className="pi-add-to-cart"><ShoppingCart /> Agregar al carrito</button>
+                            </div>
                         ) : (
                             <div className="pi-shipping-container">
-                                <span className="pi-without-stock">Producto agotado</span>
+                                <span className="pi-without-stock">Producto {!product.b2c.enabled ? "no disponible" : "agotado"}</span>
                             </div>
                         )}
                         
